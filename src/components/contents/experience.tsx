3306505865ff20d4ce4e19f22f2737716ab60823
Stefano Sechi <sechistefano@meta.com>
2025-04-19 00:11:20 +0100

    Add fourth experience entry duplicating third experience in experience.tsx

--- src/components/contents/experience.tsx
+++ src/components/contents/experience.tsx
@@ -7,30 +7,77 @@ const Experience = () => {
                 <TimeLineItem active>
                     <TimeLineItem.Title>
                         <span className="font-medium transition-all hover:text-white hover:underline hover:underline-offset-2">
-                            Frontend Developer at Polymath AI
+                        Media Buyer & Web Developer (Sole Trader)
+                        </span>
+                        <br />
+                        <span className="text-sm text-neutral-400">
+                            Freelancer
                         </span>
                         {" "}•{" "}
                         <span className="text-neutral-200">
-                            Jun 2024 - Aug 2024
+                            03/2019 - Present
                         </span>
                     </TimeLineItem.Title>
                     <TimeLineItem.Description>
-                        Worked as a frontend developer at Polymath AI, contributing to innovative solutions in the AI space.
+                    Expert freelancer in advertising, Web development and e-commerce development.                    </TimeLineItem.Description>
+                </TimeLineItem>
+
+                <TimeLineItem active>
+                    <TimeLineItem.Title>
+                        <span className="font-medium transition-all hover:text-white hover:underline hover:underline-offset-2">
+                        Analyst
+                        </span>
+                        <br />
+                        <span className="text-sm text-neutral-400">
+                            Covalen - Dublin
+                        </span>
+                        {" "}•{" "}
+                        <span className="text-neutral-200">
+                            11/2024 - Present
+                        </span>
+                    </TimeLineItem.Title>
+                    <TimeLineItem.Description>
+                    Investigating and resolving issues reported on the client’s platform, such as account support requests and
+reports of potentially abusive content.                    </TimeLineItem.Description>
+                </TimeLineItem>
+
+                <TimeLineItem>
+                    <TimeLineItem.Title>
+                        <span className="font-medium transition-all hover:text-white hover:underline hover:underline-offset-2">
+                        Trust & Safety Associate (contractor)
+                        </span>
+                        <br />
+                        <span className="text-sm text-neutral-400">
+                            Acenture
+                        </span>
+                        {" "}•{" "}
+                        <span className="text-neutral-200">
+                           04/2024 - 09/2024
+                        </span>
+                    </TimeLineItem.Title>
+                    <TimeLineItem.Description>
+                    Quality Assessment: Evaluating the accuracy, relevance, and quality of Gemini AI-generated content.
                     </TimeLineItem.Description>
                 </TimeLineItem>
 
                 <TimeLineItem last>
                     <TimeLineItem.Title>
                         <span className="font-medium transition-all hover:text-white hover:underline hover:underline-offset-2">
-                            Backend Developer (Freelance) at Saman Technosys
+                        Data Analyst Stage
+                        </span>
+                        <br />
+                        <span className="text-sm text-neutral-400">
+                            Columen
                         </span>
                         {" "}•{" "}
                         <span className="text-neutral-200">
-                            Oct 2023 - Nov 2023
+                            04/2024 - 07/2024
                         </span>
                     </TimeLineItem.Title>
                     <TimeLineItem.Description>
-                        Developed scripts with Node.js to get scrap data of profiles and store in excel. (300k profiles)
+                    Contributed to data-driven projects by performing de‐
+tailed analyses and providing recommendations based on findings.
+
                     </TimeLineItem.Description>
                 </TimeLineItem>
             </TimeLine>
